0a833511d58a93707bbc543f9f1799a1f5b2037d
Björn Mett <bjorn@mywork.com.au>
2020-09-22 13:31:34 +1000

    fixed qty calculation

--- catalog/controller/extension/payment/youpay.php
+++ catalog/controller/extension/payment/youpay.php
@@ -67,7 +67,7 @@ class ControllerExtensionPaymentYoupay extends Controller {
 						'title'         => $product_data['name'],
 						'quantity'      => (int)$order_product['quantity'],
 						'price'         => $price,
-						'total'         => $this->tax->calculate($price, $product_data['tax_class_id'], $this->config->get('config_tax'))
+						'total'         => $this->tax->calculate($price*(int)$order_product['quantity'], $product_data['tax_class_id'], $this->config->get('config_tax'))
 					)
 				);
 			}
